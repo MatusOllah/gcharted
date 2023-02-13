27cd3b28f9c8e005ca104fd08a62da04add1ca9d
MatusOllah <matusollah420@gmail.com>
2023-02-13 15:00:39 +0100

    Update numEntry.go

--- internal/ui/numEntry.go
+++ internal/ui/numEntry.go
@@ -1,6 +1,9 @@
 package ui
 
 import (
+	"strconv"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/data/validation"
 	"fyne.io/fyne/v2/driver/mobile"
 	"fyne.io/fyne/v2/widget"
@@ -10,13 +13,32 @@ type NumEntry struct {
 	widget.Entry
 }
 
-func (n *NumEntry) Keyboard() mobile.KeyboardType {
-	return mobile.NumberKeyboard
-}
-
 func NewNumEntry() *NumEntry {
 	e := &NumEntry{}
 	e.ExtendBaseWidget(e)
 	e.Validator = validation.NewRegexp(`\d`, "Must contain a number")
 	return e
+}
+
+func (e *NumEntry) TypedRune(r rune) {
+	if (r >= '0' && r <= '9') || r == '.' || r == ',' {
+		e.Entry.TypedRune(r)
+	}
+}
+
+func (e *NumEntry) TypedShortcut(shortcut fyne.Shortcut) {
+	paste, ok := shortcut.(*fyne.ShortcutPaste)
+	if !ok {
+		e.Entry.TypedShortcut(shortcut)
+		return
+	}
+
+	content := paste.Clipboard.Content()
+	if _, err := strconv.ParseFloat(content, 64); err == nil {
+		e.Entry.TypedShortcut(shortcut)
+	}
+}
+
+func (n *NumEntry) Keyboard() mobile.KeyboardType {
+	return mobile.NumberKeyboard
 }
