225be0729eb47b5b3a5177b0dec5c444f39863cd
MatusOllah <matusollah420@gmail.com>
2023-02-17 20:42:55 +0100

    export vars

--- internal/gcharted/global.go
+++ internal/gcharted/global.go
@@ -1,5 +1,16 @@
 package gcharted
 
+import "fyne.io/fyne/v2/data/binding"
+
 var (
 	Version = "1.0.0"
+
+	IsMasterMuted       bool = false
+	IsInstMuted         bool = false
+	IsVocalsMuted       bool = false
+	MasterVolumeBinding binding.Float
+	InstVolumeBinding   binding.Float
+	VocalsVolumeBinding binding.Float
+	InstPathBinding     binding.String
+	VocalsPathBinding   binding.String
 )
