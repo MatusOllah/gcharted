d64137c3eb71bfcd1130b6fda03548458859c1d9
Matúš Ollah <matusollah420@gmail.com>
2025-04-14 19:53:26 +0200

    WIP...

---
+++ internal/gui/about.go
@@ -0,0 +1,43 @@
+package gui
+
+import (
+	"image"
+	"runtime"
+
+	"github.com/AllenDang/giu"
+	"github.com/MatusOllah/gcharted/assets"
+	"github.com/MatusOllah/gcharted/internal/i18n"
+	"github.com/MatusOllah/gcharted/version"
+)
+
+var showAboutWindow bool = false
+
+func getIcon() (*image.RGBA, error) {
+	f, err := assets.FS.Open("icon.png")
+	if err != nil {
+		return nil, err
+	}
+
+	return giu.PNGToRgba(f)
+}
+
+func makeAboutWindowLoop() (func(), error) {
+	iconImg, err := getIcon()
+	if err != nil {
+		return nil, err
+	}
+
+	return func() {
+		if showAboutWindow {
+			giu.Window(i18n.L("About")).IsOpen(&showAboutWindow).Size(320, 240).Flags(giu.WindowFlagsNoResize).Layout(
+				giu.Align(giu.AlignCenter).To(giu.ImageWithRgba(iconImg).Size(64, 64)),
+				giu.Label(i18n.LT("GChartedVersion", map[string]any{"Version": version.Version})),
+				giu.Label(i18n.LT("GoVersion", map[string]any{"Version": runtime.Version(), "GOOS": runtime.GOOS, "GOARCH": runtime.GOARCH})),
+				giu.Label(""),
+				giu.Label(i18n.L("MadeWithLove")),
+				giu.Label("Copyright (c) 2025 Matúš Ollah"),
+				giu.Label(i18n.L("License")),
+			)
+		}
+	}, nil
+}
---
+++ internal/gui/menubar.go
@@ -0,0 +1,31 @@
+package gui
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/AllenDang/giu"
+	"github.com/MatusOllah/gcharted/internal/i18n"
+)
+
+func menuBarLoop() {
+	giu.MainMenuBar().Layout(
+		giu.Menu(i18n.L("File")).Layout(
+			giu.MenuItem(i18n.L("Exit")).OnClick(func() {
+				slog.Debug("clicked exit menu item, exiting")
+				os.Exit(0)
+			}),
+		),
+		giu.Menu(i18n.L("Tools")).Layout(
+			giu.MenuItem(i18n.L("ConvertVorbis")).OnClick(func() {
+				slog.Debug("clicked convert to vorbis menu item")
+			}),
+		),
+		giu.Menu(i18n.L("Help")).Layout(
+			giu.MenuItem(i18n.L("About")).OnClick(func() {
+				slog.Debug("clicked about menu item")
+				showAboutWindow = true
+			}),
+		),
+	).Build()
+}
