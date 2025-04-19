c1a782510213a5268982a82a239fda2d493086b9
Matúš Ollah <matusollah420@gmail.com>
2025-04-19 22:47:16 +0200

    add toolbar

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -20,21 +20,24 @@ func MakeWindowLoop() (func(), error) {
 		giu.PushWindowPadding(0, 0)
 		giu.SingleWindowWithMenuBar().Layout(giu.CSSTag("masterwindow").To(
 			menuBar(),
-			giu.Child().ID("container").Size(-1, -25).Border(false).Layout(
-				giu.Custom(func() {
-					main := giu.Label("TODO: main content")
+			giu.Child().Size(-1, -30).Border(false).Layout(
+				giu.Child().ID("toolbar").Size(-1, 30).Flags(giu.WindowFlagsNoDecoration).Layout(giu.CSSTag("toolbar").To(toolBar())),
+				giu.Child().ID("container").Size(-1, -1).Border(false).Layout(
+					giu.Custom(func() {
+						main := giu.Label("TODO: main content")
 
-					var widget giu.Widget = main
+						var widget giu.Widget = main
 
-					if showBottomSiderbar {
-						widget = giu.SplitLayout(giu.DirectionHorizontal, &sashPosBottomSidebar, main, bottomSidebar())
-					}
-					if showRightSiderbar {
-						widget = giu.SplitLayout(giu.DirectionVertical, &sashPosRightSidebar, widget, rightSidebar())
-					}
+						if showBottomSiderbar {
+							widget = giu.SplitLayout(giu.DirectionHorizontal, &sashPosBottomSidebar, main, bottomSidebar())
+						}
+						if showRightSiderbar {
+							widget = giu.SplitLayout(giu.DirectionVertical, &sashPosRightSidebar, widget, rightSidebar())
+						}
 
-					widget.Build()
-				}),
+						widget.Build()
+					}),
+				),
 			),
 			giu.Child().ID("statusbar").Size(-1, -1).Flags(giu.WindowFlagsNoDecoration).Layout(giu.CSSTag("statusbar").To(statusBar())),
 		))
---
+++ internal/gui/toolbar.go
@@ -0,0 +1,9 @@
+package gui
+
+import "github.com/AllenDang/giu"
+
+func toolBar() giu.Widget {
+	return giu.Row(
+		giu.Label("TODO: toolbar"),
+	)
+}
