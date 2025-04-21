b40390e0d816a70308223098b2e0797f2e6088ee
Matúš Ollah <matusollah420@gmail.com>
2025-04-21 23:09:16 +0200

    WIP...

--- internal/gui/bottomSidebar.go
+++ internal/gui/bottomSidebar.go
@@ -1,7 +1,21 @@
 package gui
 
-import "github.com/AllenDang/giu"
+import (
+	"github.com/AllenDang/giu"
+	"github.com/MatusOllah/gcharted/internal/i18n"
+)
 
 func bottomSidebar() giu.Widget {
-	return giu.Label("TODO: bottom sidebar")
+	return giu.TabBar().Flags(giu.TabBarFlagsReorderable).TabItems(
+		giu.TabItem(i18n.L("AudioMixer")).Layout(audioMixer()),
+		giu.TabItem(i18n.L("ProjectDescription")).Layout(projDescription()),
+	)
+}
+
+func audioMixer() giu.Widget {
+	return giu.Label("TODO: audio mixer")
+}
+
+func projDescription() giu.Widget {
+	return giu.Label("TODO: project description")
 }
